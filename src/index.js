a7558ef572ffd3c980e444ca0f0d790178dea347
Jose Banuelos <JBanuelos@esri.com>
2023-04-26 19:45:13 -0700

    Creating DraftPage and moving taps to the App level

--- src/index.js
+++ src/index.js
@@ -3,30 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
-
-const BrowserApp = () => {
-  return (
-    <Tabs
-      defaultActiveKey="jose"
-      id="uncontrolled-tab-example"
-      className="mb-3"
-    >
-      <Tab eventKey="jose" title="Jose">
-        <App user="jose" />
-      </Tab>
-      <Tab eventKey="noah" title="Noah">
-        <App user="noah" />
-      </Tab>
-    </Tabs>
-  );
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserApp />
+    <App user="jose" />
   </React.StrictMode>
 );
 
